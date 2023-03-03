e9d3a8d473732f1ac9d83f34121ed02d42f77267
aj2000 <hi@ajaysharma.dev>
2023-03-04 00:36:57 +0530

    readme and boilerplate update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function findLargest(a, b, c) {
+  //your code here
+}
+
+const num1 = parseInt(prompt("Enter First Number."));
+const num2 = parseInt(prompt("Enter Second Number."));
+const num3 = parseInt(prompt("Enter Third Number."));
+alert(findLargest(num1, num2, num3));
