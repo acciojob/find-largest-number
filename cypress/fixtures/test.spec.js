58863f9363f448a5c0971243a2d851d163758ebc
Abhinaba Roy Choudhury <36771803+Abhi8016@users.noreply.github.com>
2024-12-25 16:14:05 +0530

    Update test.spec.js

--- cypress/fixtures/test.spec.js
+++ cypress/fixtures/test.spec.js
@@ -1,1 +1,8 @@
-
+describe("test of relative list", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:8080");
+    });
+    it("should check body", () => {
+      cy.get("body");
+    });
+  });
