bd7abd75446c767f2a48338a0976c06c3b661cef
Abhinaba Roy Choudhury <36771803+Abhi8016@users.noreply.github.com>
2024-12-25 16:18:47 +0530

    Delete cypress/fixtures/test.spec.js

--- cypress/fixtures/test.spec.js
+++
@@ -1,8 +0,0 @@
-describe("test of relative list", () => {
-    beforeEach(() => {
-      cy.visit("http://localhost:8080");
-    });
-    it("should check body", () => {
-      cy.get("body");
-    });
-  });
